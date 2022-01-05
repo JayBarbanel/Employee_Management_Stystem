eb3770048aa4db0be5e2e927b750292ec193dc68
Jay <jbarbanel531@gmail.com>
2022-01-04 17:33:13 -0800

    updated bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
